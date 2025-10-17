7e2ecafa5c6fbef8709da784f008391835025262
josephboi123 <jevaldez101@student.hbuhsd.edu>
2025-10-17 18:20:29 +0000

    commit

--- Main.java
+++ Main.java
@@ -5,5 +5,13 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+
+    int count = 3; 
+    while (count <= 25)
+
+    {  System.out.println(count);
+    
+    count = count + 2; 
+    }
   }
 }
